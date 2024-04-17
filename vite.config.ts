8d926eff63613e62acc92515daf33b9501c69eb2
qiyang20 <70252089+qiyang20@users.noreply.github.com>
2024-04-17 16:53:53 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,16 +6,19 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base:'/vite-vue3-github-actions-demo/',
-  plugins: [
-    vue(),
-    vueJsx(),
-    VueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig({mode}=>{
+  const isProd = mode=== 'production'
+  return{
+    base: isProd ?'/vite-vue3-github-actions-demo/' : './',
+    plugins: [
+      vue(),
+      vueJsx(),
+      VueDevTools(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
     }
   }
 })
