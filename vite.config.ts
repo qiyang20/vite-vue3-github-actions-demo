d2cc5569ecd0122f581da2eb15c7450883ef8ac4
qiyang20 <1079359761@qq.com>
2024-04-17 17:01:03 +0800

    修改vite.config.js

--- vite.config.ts
+++ vite.config.ts
@@ -6,15 +6,11 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
-export default defineConfig(({mode})=>{
-  const isProd = mode=== 'production'
-  return{
-    base: isProd ?'/vite-vue3-github-actions-demo/' : './',
-    plugins: [
-      vue(),
-      vueJsx(),
-      VueDevTools(),
-    ],
+export default defineConfig(({ mode }) => {
+  const isProd = mode === 'production'
+  return {
+    base: isProd ? '/vite-vue3-github-actions-demo/' : './',
+    plugins: [vue(), vueJsx(), VueDevTools()],
     resolve: {
       alias: {
         '@': fileURLToPath(new URL('./src', import.meta.url))
